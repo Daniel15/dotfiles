eed284f165a77242246ce92b510c853940dc11e5
danlo <danlo@fb.com>
2024-05-30 17:08:34 -0700

    Add some initial config from work devserver

--- private_dot_config/zsh/aliases.sh
+++ private_dot_config/zsh/aliases.sh
@@ -1,3 +1,14 @@
 # Aliases
 
 alias ll="ls -lah"
+
+if [[ $DANIEL15_ENV == "work-devserver" ]]; then
+# Aliases - work devservers
+  alias hgdf='hg diff --color=always | diff-so-fancy | less -FR'
+  alias hgrs='hg pull && hg rebase --dest remote/stable --source'
+  alias hgrh='hg rebase --dest . --source'
+  alias hgus='hg pull && hg update remote/stable'
+
+  alias jfsd='jf submit --draft'
+  alias jfssd='jf submit --stack --draft'
+fi
