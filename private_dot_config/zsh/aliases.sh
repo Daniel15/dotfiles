b71592385efeb90de2b45238af8194db5f312b7b
Daniel Lo Nigro <danlo@meta.com>
2024-05-31 17:12:14 -0700

    Modified hgrs alias to update to new commit

--- private_dot_config/zsh/aliases.sh
+++ private_dot_config/zsh/aliases.sh
@@ -4,13 +4,22 @@ alias ll="ls -lah"
 
 if [[ $DANIEL15_ENV == "work-devserver" ]]; then
 # Aliases - work devservers
+  # hg diff, fancy
   alias hgdf='hg diff --color=always | diff-so-fancy | less -FR'
-  alias hgrs='hg pull && hg rebase --dest remote/stable --source'
+  # hg rebase another commit here
   alias hgrh='hg rebase --dest . --source'
+  # hg update to stable
   alias hgus='hg pull && hg update remote/stable'
 
+  # jf submit as draft
   alias jfsd='jf submit --draft'
+  # jf submit stack as a draft
   alias jfssd='jf submit --stack --draft'
+
+  # hg rebase to stable, and update to new commit
+  function hgrs() {
+    hg pull && hg rebase --source "$1" --dest remote/stable && hg go "successors($1)"
+  }
 
   # Basic fastmod to rename a symbol based on BigGrep results
   function fastmod-rename() {
